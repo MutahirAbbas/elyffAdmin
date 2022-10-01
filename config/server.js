5b6562aed01155cc7d9870701844393ed3104068
Mutahir <muhammad.mutahir@pitb.gov.pk>
2022-10-01 15:58:50 +0500

    Fixes

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: 'zeJCn4R378czy3eNrRuiGg==,U/RhW7EMx2miTlJp4JDd/Q==,w/KWjvKfH+P7MCBAKSQCHQ==,xdo0m9WMnZ92Nj+f/B+DXg==',
+    keys: env.array('APP_KEYS'),
   },
 });
